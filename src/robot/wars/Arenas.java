03f7f493eee427b929c7eded4afb201463823614
Tim Harshman <goteamtim+git@gmail.com>
2014-04-24 07:03:24 -0700

    Arena class
    Arena class will define what the various arenas the robots will battle
    in.

---
+++ src/robot/wars/Arenas.java
@@ -0,0 +1,14 @@
+/*
+ * This is the arenas class.  This class holds the various arenas that the robots battle in.
+ * Each arena has its own pitfalls and obstacles to deal with.  Each arena has a set battle
+ * structure for the robots to deal with.
+ */
+package robot.wars;
+
+/**
+ *
+ * @author Goteamtim
+ */
+public class Arenas {
+    
+}
